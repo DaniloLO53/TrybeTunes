ed45e9380010b097503c7ae36bd5056e5ebef282
Danilo Leão <olin_ad@hotmail.com>
2022-12-03 09:55:28 -0300

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <StyledApp>
+      oi
+    </StyledApp>
+  );
+}
+
+const StyledApp = styled.div`
+
+`;
+
+export default App;
